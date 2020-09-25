848b8d22ceb93e9e3faaad715aed8f4e6b88e170
coopergoldenholt <coopergoldenholt12@gmail.com>
2020-09-25 16:11:07 -0600

    displaying all help sections

--- app/screens/Dashboard/components/Help/Help.js
+++ app/screens/Dashboard/components/Help/Help.js
@@ -37,11 +37,136 @@ const Help = (props) => {
       );
     } else null;
   };
+  const checkRebounds = () => {
+    const goalRebound = goals.orbAvg + goals.drbAvg;
+    const averageRebound = averages.orbAvg + averages.drbAvg;
+
+    if (goalRebound > averageRebound) {
+      return (
+        <>
+          <View style={styles.cardStackContainer}>
+            <Text style={styles.textNoMarginBottom}>
+              You are not reaching your rebound goals.
+            </Text>
+            <Text style={styles.text}>
+              Check out these drills to get better!
+            </Text>
+            <CardStack />
+          </View>
+        </>
+      );
+    } else null;
+  };
+  const checkThreePerc = () => {
+    const goalThreePerc =
+      Math.round((goals.threeMAvg / goals.threeAAvg) * 10000) / 100;
+    const averageThreePerc =
+      Math.round((averages.threeMAvg / averages.threeAAvg) * 10000) / 100;
+
+    if (goalThreePerc > averageThreePerc) {
+      return (
+        <>
+          <View style={styles.cardStackContainer}>
+            <Text style={styles.textNoMarginBottom}>
+              You are not reaching your three point percentage goals.
+            </Text>
+            <Text style={styles.text}>
+              Check out these drills to get better!
+            </Text>
+            <CardStack />
+          </View>
+        </>
+      );
+    } else null;
+  };
+  const checkFTPerc = () => {
+    const goalFTPerc = Math.round((goals.ftmAvg / goals.ftaAvg) * 10000) / 100;
+    const averageFTPerc =
+      Math.round((averages.ftmAvg / averages.ftaAvg) * 10000) / 100;
+
+    if (goalFTPerc > averageFTPerc) {
+      return (
+        <>
+          <View style={styles.cardStackContainer}>
+            <Text style={styles.textNoMarginBottom}>
+              You are not reaching your free throw percentage goals.
+            </Text>
+            <Text style={styles.text}>
+              Check out these drills to get better!
+            </Text>
+            <CardStack />
+          </View>
+        </>
+      );
+    } else null;
+  };
+  const checkFieldPerc = () => {
+    const goalFieldPerc =
+      Math.round((goals.fgAvg / goals.fgAAvg) * 10000) / 100;
+    const averageFieldPerc =
+      Math.round((averages.fgAvg / averages.fgAAvg) * 10000) / 100;
+
+    if (goalFieldPerc > averageFieldPerc) {
+      return (
+        <>
+          <View style={styles.cardStackContainer}>
+            <Text style={styles.textNoMarginBottom}>
+              You are not reaching your three point percentage goals.
+            </Text>
+            <Text style={styles.text}>
+              Check out these drills to get better!
+            </Text>
+            <CardStack />
+          </View>
+        </>
+      );
+    } else null;
+  };
+  const checkTO = () => {
+    if (goals.tovAvg > averages.tovAvg) {
+      return (
+        <>
+          <View style={styles.cardStackContainer}>
+            <Text style={styles.textNoMarginBottom}>
+              You are not reaching your turover goals.
+            </Text>
+            <Text style={styles.text}>
+              Check out these drills to get better!
+            </Text>
+            <CardStack />
+          </View>
+        </>
+      );
+    } else null;
+  };
+  const checkStl = () => {
+    if (goals.stlAvg > averages.stlAvg) {
+      return (
+        <>
+          <View style={styles.cardStackContainer}>
+            <Text style={styles.textNoMarginBottom}>
+              You are not reaching your steal goals.
+            </Text>
+            <Text style={styles.text}>
+              Check out these drills to get better!
+            </Text>
+            <CardStack />
+          </View>
+        </>
+      );
+    } else null;
+  };
 
   return (
     <View style={styles.container}>
+      {checkFieldPerc()}
+      {checkThreePerc()}
+      {checkFTPerc()}
       {checkAst()}
+      {checkRebounds()}
       {checkBlock()}
+      {checkStl()}
+      {checkTO()}
     </View>
   );
 };
